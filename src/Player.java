c5e2a73713f1694d82c0a753c9c131a3f2c114ff
n3ilm4lh0tra <malhotra@spsejecna.cz>
2024-03-15 22:42:41 +0100

    move method in making

--- src/Player.java
+++ src/Player.java
@@ -1,12 +1,18 @@
-public class Player {
+import java.util.Scanner;
 
+public class Player {
     private String turn;
-    private String x = "X";
+    private final String x = "X";
     private int col;
     private int row;
+    private boolean taken;
 
     public void makeMove(){
-
+        Scanner sc = new Scanner(System.in);
+        System.out.print("MAKE_MOVE\n INPUT_COLUMN:");
+        col = sc.nextInt();
+        System.out.print("INPUT_ROW");
+        row = sc.nextInt();
     }
 
     public String getTurn() {
