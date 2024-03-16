40f9e581492d617338f8f8f864efe4041e1cea19
n3ilm4lh0tra <126697960+n3ilm4lh0tra@users.noreply.github.com>
2024-03-16 21:48:39 +0100

    Update PlayingBoard.java

--- src/PlayingBoard.java
+++ src/PlayingBoard.java
@@ -6,6 +6,8 @@ public class PlayingBoard {
     private final int columns = 3;
 
     private final Space[] board = new Space[9];
+    private String[] spaceString = new String[9];
+    private String board = "   |   |\n " + spaceString[0];
 
     public void startTheGame(){
 
@@ -18,7 +20,7 @@ public class PlayingBoard {
     public void move(){
         Scanner sc = new Scanner(System.in);
         System.out.println("""
-                """);
+                CHOOSE_A_PLACE_ON_THE_BOARD""");
     }
 
     public String checkWinner(){
