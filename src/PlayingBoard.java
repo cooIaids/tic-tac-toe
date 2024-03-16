0d4f0382425ed3c9ada34a1131c42c34fd00b497
n3ilm4lh0tra <malhotra@spsejecna.cz>
2024-03-16 22:52:08 +0100

    added move + removed Player.java

--- src/PlayingBoard.java
+++ src/PlayingBoard.java
@@ -1,34 +1,39 @@
 import java.util.Scanner;
 
 public class PlayingBoard {
+    Scanner sc = new Scanner(System.in);
     private final Space[] board = new Space[9];
-    private String[] spaceString = new String[9];
+    private final String[] spaceString = new String[9];
     String board_g;
-    public void createBoard(){
+    private int x;
+
+    public void createBoard() {
         String index;
-        for(int i = 0; i<spaceString.length; i++){
-            index = String.valueOf(i+1);
+        for (int i = 0; i < spaceString.length; i++) {
+            index = String.valueOf(i + 1);
             spaceString[i] = index;
         }
         board_g = "   |   |\n " +
                 spaceString[0] + " | " + spaceString[1] + " | " + spaceString[2] +
-                "\n___|___|___\n "+
+                "\n___|___|___\n " +
                 spaceString[3] + " | " + spaceString[4] + " | " + spaceString[5] +
-                "\n___|___|___\n "+
+                "\n___|___|___\n " +
                 spaceString[6] + " | " + spaceString[7] + " | " + spaceString[8] +
                 "\n   |   |   \n ";
         System.out.println(board_g);
     }
-    public void updateBoard(){
+
+    public void updateBoard() {
         board_g = "   |   |\n " +
                 spaceString[0] + " | " + spaceString[1] + " | " + spaceString[2] +
-                "\n___|___|___\n "+
+                "\n___|___|___\n " +
                 spaceString[3] + " | " + spaceString[4] + " | " + spaceString[5] +
-                "\n___|___|___\n "+
+                "\n___|___|___\n " +
                 spaceString[6] + " | " + spaceString[7] + " | " + spaceString[8] +
                 "\n   |   |   \n ";
     }
-    public void startTheGame(){
+
+    public void startTheGame() {
 
         System.out.println("""
                 ==========================
@@ -37,28 +42,23 @@ public class PlayingBoard {
     }
 
     public void move(){
-        Scanner sc = new Scanner(System.in);
         System.out.println(board_g);
         System.out.print("CHOOSE_A_PLACE_ON_THE_BOARD 1-9: ");
-        int x = sc.nextInt();
-        if(x>=1&&x<=9){
-            spaceString[x-1] = "x";
-        }else{
+        x = sc.nextInt();
+        if (x >= 1 && x <= 9 && !board[x - 1].isTakenByComp() && !board[x - 1].isTakenByPlayer()) {
+            spaceString[x - 1] = "x";
+            board[x - 1] = new Space(true, false);
+        } else {
             System.out.println("WRONG");
         }
         updateBoard();
         System.out.println(board_g);
+
     }
 
-    public String checkWinner(){
+    public String checkWinner() {
         return null;
     }
-
-
-
-
-
-
 
 
 }
--- src/Space.java
+++ src/Space.java
@@ -1,15 +1,25 @@
 public class Space {
     private boolean takenByPlayer;
     private boolean takenByComp;
-    private int col;
-    private int row;
-    private int[][] coordinates = new int[3][3];
 
-    public Space(boolean takenByPlayer, boolean takenByComp, int col, int row, int[][] coordinates) {
+    public Space(boolean takenByPlayer, boolean takenByComp) {
         this.takenByPlayer = takenByPlayer;
         this.takenByComp = takenByComp;
-        this.col = col;
-        this.row = row;
-        this.coordinates = coordinates;
+    }
+
+    public boolean isTakenByPlayer() {
+        return takenByPlayer;
+    }
+
+    public void setTakenByPlayer(boolean takenByPlayer) {
+        this.takenByPlayer = takenByPlayer;
+    }
+
+    public boolean isTakenByComp() {
+        return takenByComp;
+    }
+
+    public void setTakenByComp(boolean takenByComp) {
+        this.takenByComp = takenByComp;
     }
 }
