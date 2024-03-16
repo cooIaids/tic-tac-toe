440279898bec4ef78704632067658bfe306444be
n3ilm4lh0tra <malhotra@spsejecna.cz>
2024-03-16 10:46:12 +0100

    recreating board

--- src/PlayingBoard.java
+++ src/PlayingBoard.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class PlayingBoard {
 
     private final int rows = 3;
@@ -5,16 +7,18 @@ public class PlayingBoard {
 
     private final Space[] board = new Space[9];
 
-
-
     public void startTheGame(){
 
         System.out.println("""
                 ==========================
                 Welcome to 3x3 Tic Tac Toe!
                 ==========================""");
+    }
 
-
+    public void move(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("""
+                """);
     }
 
     public String checkWinner(){
